4421f68463549ff463b5384ae3b29a88b811c99b
Diego Morais <diego97.git@gmail.com>
2025-02-24 14:41:27 -0300

    definino rotas do projeto

---
+++ src/pages/Conta/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const Conta=()=>{
+    return <div>
+        conta
+    </div>
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { Routes as Router,Route } from "react-router-dom"
+import Home from "../pages/Home"
+import Products from "../pages/Products"
+import {Cart} from "../pages/Cart"
+import {Conta} from "../pages/Conta"
+import { Register } from "../pages/Register"
+import { Login } from "../pages/Login"
+
+
+
+export const Routes=()=>{
+    return <Router >
+        <Route path="/" Component={Home}/>
+        <Route path="/produtos" Component={Products}/>
+        <Route path="/carrinho" Component={Cart}/>
+        <Route path="/minha-conta" Component={Conta}/>
+        <Route path="/cadastro" Component={Register}/>
+        <Route path="/login" Component={Login}/>
+    </Router>
+}
